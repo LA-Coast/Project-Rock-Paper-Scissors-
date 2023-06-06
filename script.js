53639093b75e6950285072d4f3e9a04efbf46516
leticiaac-costa <122683252+LA-Coast@users.noreply.github.com>
2023-06-06 19:14:02 -0300

    Create function round and game

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
-/*  Eu quero fazer um jogo de pedra papel e tesoura
-    Nele o o usuario vai jogar contra o computador.
-    Serão 5 partidas, sendo que quem ganha a rodada
-    é aquele que ganho mais de duas partidas.
-    
-    As etapas para esse programa funcinar são:
-    Pedir o input do usuario.
-    Receber um input aleatorio do computador.
-    Comparar os paramentros dos inputs do 
-    usuario e computador para determinar o 
-    vecedor.
-    Retornar e guardar quem ganhou a partida.
-    Repertir os passos anteriores 5 vezes.
-    Com os resultados guardados das partidas aplicar
-    os paramentros para decidir o jogador ganhador.*/
-
 function playerSelection() {
-    return prompt("Rock, Paper or Scissors?").toLowerCase();
+    const playerOption = prompt("Rock, Paper or Scissors?").toLowerCase();
+    return playerOption;
 }
-    
+
 function getComputerChoice() {
     const shapes = ["rock", "paper", "scissors"]; 
     const random = Math.floor(Math.random() * shapes.length);
     return shapes[random];
 }
 
-function round(getComputerChoice, playerSelection) {
-    if 
+function round() {
+    const computerChoice = getComputerChoice();
+    const playerChoice = playerSelection();
+
+    console.log(`The player selected: ${playerChoice}`);
+    console.log(`The computer selected: ${computerChoice}`); 
+
+    if (computerChoice === playerChoice){
+        console.log("It is a tie!");
+        return "It is a tie!";
+    }
+
+    else if (
+    (computerChoice === "rock" && playerChoice === "scissors") ||
+    (computerChoice === "scissors" && playerChoice === "paper") ||
+    (computerChoice === "paper" && playerChoice === "rock")
+    ) {
+        console.log("Computer wins!");
+        return "Computer wins!"
+    }   
+
+    else {
+        console.log("Player wins!");
+        return "Player wins!"
+    }
 }
-rock > scissors 
-scissors > paper
-paper > rock 
+
+function game(){
+    let playerScore = 0
+    let computerScore = 0
+    
+    for (let i = 0; i < 5; i++) {
+        const score = round()
+    if (score === "Computer wins!"){
+        computerScore++;
+    } 
+    else if (score === "Player wins!"){
+        playerScore++;
+    }  
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+    }
+
+    if (playerScore > computerScore){
+        console.log("Player win the game!");
+    } else if (computerScore > playerScore){
+        console.log("Computer win the game!")
+    } else {
+        console.log("It is a Tie!")
+    }
+}
+
+game()
+    
